8aa7f61ae0b7ef316ee013dd8148edb17288141d
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-10-07 14:39:20 +0530

    Removed Wrangler.toml

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,26 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+app.post('/api/v1/signup', (c) => {
+  return c.text('Sign up page.')
+})
+
+app.post('/api/v1/signin', (c) => {
+  return c.text('Sign in page.')
+})
+
+app.get('/api/v1/blog', (c) => {
+  return c.text('Blog.')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('Blog-1.')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Blog.')
+})
+
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
